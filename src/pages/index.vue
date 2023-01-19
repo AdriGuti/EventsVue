1e805edf17a0041c7a3ba256e65a8db54ad81dbc
AdriGuti <adriangutierrezsoria@gmail.com>
2023-01-19 19:03:49 +0100

    developing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,12 @@
   <v-container>
     <v-row>
       <v-col>
-        
+        <h1>
+          Aqui estem
+        </h1>
+        <h2>
+          Com anem?
+        </h2>
       </v-col>
     </v-row>
   </v-container>    
